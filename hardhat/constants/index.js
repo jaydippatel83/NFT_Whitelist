910b54d264f9378c3c90d1b64f35f39841f5e241
Jaydippatel <macbook@MacBooks-MacBook-Pro-2.local>
2022-12-14 17:39:21 +0530

    deploy and mint

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,8 +1,8 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xb391b4Fa138594Cc0D65d309a99B077f988b918f";
-// URL to extract Metadata for a Crypto Dev NFT
+// const WHITELIST_CONTRACT_ADDRESS = "0xb391b4Fa138594Cc0D65d309a99B077f988b918f";
+const WHITELIST_CONTRACT_ADDRESS = "0xea2826E7f3ccFD2b8338aeca7A459574259634A6";
+// URL to extract Metadata for a Crypto Dev NFT 
 
-// Crypto Devs Contract Address: 0xB2540C00095E1b18A334b45f61A0A9e35027dD6E
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/"; 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
